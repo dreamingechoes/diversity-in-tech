3d26d40eeceed983905b9d0d0383d113b702c59a
Ivan Gonzalez <hi@dreamingecho.es>
2018-02-10 17:49:21 +0100

    Update mix.exs with new dependencies

--- mix.exs
+++ mix.exs
@@ -5,10 +5,10 @@ defmodule DiversityInTech.Mixfile do
     [
       app: :diversity_in_tech,
       version: "0.0.1",
-      elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixir: "~> 1.6",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -26,21 +26,39 @@ defmodule DiversityInTech.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.10"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      # Default Phoenix stuff
+      {:cowboy, "~> 1.1.2"},
+      {:gettext, "~> 0.13.1"},
+      {:phoenix, "~> 1.3.0", override: true},
+      {:phoenix_ecto, "~> 3.3.0"},
+      {:postgrex, ">= 0.13.3"},
+      {:phoenix_pubsub, "~> 1.0.2"},
+
+      # Frontend
+      {:phoenix_html, "~> 2.10.4"},
+      {:phoenix_live_reload, "~> 1.1.1", only: :dev},
+
+      # Code analysis
+      {:credo, "~> 0.9.0-rc1", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
+
+      # Code documentation
+      {:ex_doc, "~> 0.16.4", only: :dev, runtime: false},
+
+      # Deploying
+      {:distillery, "~> 1.5.1", runtime: false},
+
+      # Testing and seeding data
+      {:faker, "~> 0.9.0"},
+      {:excoveralls, "~> 0.7.3", only: [:test]},
+      {:exvcr, "~> 0.9.0", only: [:test]}
     ]
   end
 
@@ -54,7 +72,7 @@ defmodule DiversityInTech.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
