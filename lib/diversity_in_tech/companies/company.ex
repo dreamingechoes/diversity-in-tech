42840220139d9a581f2787a022c6e8624a64b3bf
Ivan Gonzalez <hi@dreamingecho.es>
2018-03-11 22:56:37 +0100

    Add generate_slug function and refactor in company model

--- lib/diversity_in_tech/companies/company.ex
+++ lib/diversity_in_tech/companies/company.ex
@@ -4,18 +4,39 @@ defmodule DiversityInTech.Companies.Company do
   alias DiversityInTech.Companies.Company
 
   schema "companies" do
+    field(:name, :string)
+    field(:slug, :string)
     field(:description, :string)
     field(:logo, :string)
-    field(:name, :string)
     field(:website, :string)
 
     timestamps()
   end
 
+  # Changeset cast params
+  @params [:name, :description, :logo, :website]
+  @required [:name]
+
   @doc false
   def changeset(%Company{} = company, attrs) do
     company
-    |> cast(attrs, [:name, :description, :logo, :website])
-    |> validate_required([:name, :description, :logo, :website])
+    |> cast(attrs, @params)
+    |> validate_required(@required)
+    |> generate_slug()
+  end
+
+  defp generate_slug(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{name: name}} ->
+        slug =
+          name
+          |> String.downcase()
+          |> String.replace(" ", "-")
+
+        put_change(current_changeset, :slug, slug)
+
+      _ ->
+        current_changeset
+    end
   end
 end
