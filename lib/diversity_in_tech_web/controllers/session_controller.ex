bfb34e34ec5a426cbfa0afb6011118898fd068e9
Ivan Gonzalez <hi@dreamingecho.es>
2018-03-18 21:48:06 +0100

    Add refactor to session controller

--- lib/diversity_in_tech_web/controllers/session_controller.ex
+++ lib/diversity_in_tech_web/controllers/session_controller.ex
@@ -1,15 +1,34 @@
 defmodule DiversityInTechWeb.SessionController do
   use DiversityInTechWeb, :controller
+  alias DiversityInTech.Guardian
 
   plug(:put_layout, "auth.html")
 
-  def new(conn, _) do
-    render(conn, "new.html")
-  end
+  def new(conn, _), do: render(conn, "new.html")
 
-  def create(conn, %{"session" => session_params}) do
+  def create(conn, %{
+        "session" => %{
+          "username_or_email" => username_or_email,
+          "password" => password
+        }
+      }) do
+    case Guardian.authenticate_user(username_or_email, password) do
+      {:ok, user} ->
+        conn
+        |> Guardian.Plug.sign_in(user)
+        |> put_flash(:success, gettext("Welcome to Diversity In Tech!"))
+        |> redirect(to: page_path(conn, :index))
+
+      {:error, message} ->
+        conn
+        |> put_flash(:error, message)
+        |> redirect(to: session_path(conn, :new))
+    end
   end
 
   def delete(conn, _) do
+    conn
+    |> Guardian.Plug.sign_out()
+    |> redirect(to: page_path(conn, :index))
   end
 end
