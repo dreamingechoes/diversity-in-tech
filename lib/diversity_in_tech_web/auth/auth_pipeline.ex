820514d088720900f402c8ce6c0284291bccb726
Ivan Gonzalez <hi@dreamingecho.es>
2018-03-18 21:47:33 +0100

    Add refactor to auth_pipeline module

--- lib/diversity_in_tech_web/auth/auth_pipeline.ex
+++ lib/diversity_in_tech_web/auth/auth_pipeline.ex
@@ -4,7 +4,10 @@ defmodule DiversityInTechWeb.Guardian.AuthPipeline do
     module: DiversityInTech.Guardian,
     error_handler: DiversityInTech.Guardian.AuthErrorHandler
 
-  plug(Guardian.Plug.VerifySession)
-  plug(Guardian.Plug.VerifyHeader)
-  plug(Guardian.Plug.EnsureAuthenticated)
+  # If there is a session token, validate it
+  plug(Guardian.Plug.VerifySession, claims: %{"typ" => "access"})
+  # If there is an authorization header, validate it
+  plug(Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"})
+  # Load the user if either of the verifications worked
+  plug(Guardian.Plug.LoadResource, allow_blank: true)
 end
