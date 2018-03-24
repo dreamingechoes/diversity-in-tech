2dd9d83faddb0b57feae5317fa80d45fce88124b
Ivan Gonzalez <hi@dreamingecho.es>
2018-03-24 20:50:42 +0100

    Add right formatting to config.exs with mix format

--- config/config.exs
+++ config/config.exs
@@ -29,8 +29,7 @@ config :diversity_in_tech, DiversityInTech.Guardian,
 config :arc, storage: Arc.Storage.Local
 
 # Pagination configuration
-config :scrivener_html,
-  routes_helper: DiversityInTechWeb.Router.Helpers
+config :scrivener_html, routes_helper: DiversityInTechWeb.Router.Helpers
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
